639596c3f1a32213aed34cfa0f7f618a29c99492
Kevin Le <lekevin42@yahoo.com>
2016-12-02 15:00:28 -0800

    Remove main duplicate; group text together

--- summarizer.py
+++ summarizer.py
@@ -7,6 +7,7 @@ class Summarizer:
 		self.url = url
 		self.keyword_limit = keyword_limit
 		self.sentence_limit = sentence_limit
+		self.sent_count = None
 
 	def clean_text(self):
 		lines = []
@@ -22,7 +23,7 @@ class Summarizer:
 			script.extract()  
 
 		text = soup.get_text()
-
+		
 		#remove leading and trailing space on every line
 		for line in text.splitlines():
 			lines.append(line.strip())
@@ -50,6 +51,12 @@ class Summarizer:
 				elif word == (ignored_word[0:1].upper() + ignored_word[1:]).strip():
 					return False
 					
+				elif word == (ignored_word.replace("\n", ",\n").strip()):
+					return False
+					
+				elif word == (ignored_word.replace("\n", ".\n").strip()):
+					return False
+					
 		return True
 		
 	def grab_keywords(self, text):
@@ -60,15 +67,17 @@ class Summarizer:
 		for sent in text:
 			split_text = sent.split()
 			for word in split_text:
-				if self.ignored_word(word):
+				if self.ignored_word(word) and word[0].isupper():
 					if word in dict:
 						dict[word] += 1
 					
 					else:
 						dict[word] = 0
+						
+		dict = self.check_duplicates(dict)
 				
 		dict = OrderedDict(sorted(dict.items(), key=lambda t: t[1], reverse=True))
-				
+			
 		for k, v in dict.items():
 			if counter != self.keyword_limit:
 				keywords.append(k)
@@ -76,22 +85,61 @@ class Summarizer:
 		
 		return keywords
 
+	def check_duplicates(self, dict):
+		count = 0
+		dup_list = []
+		
+		dict = OrderedDict(sorted(dict.items(), key=lambda t: t[1], reverse=True))
+		
+		dup_list.append(list(dict.items())[0][0])
+
+		for k, v in dict.items():
+			for word in dup_list:
+				if word != k:
+					if k.find(word) != -1:
+						dict[k] = 0
+		
+		dict = OrderedDict(sorted(dict.items(), key=lambda t: t[1], reverse=True))
+		return dict
+		
+		
 	def grab_summary(self, text):
 		summary = []
 
+		text = self.mark_end_of_paragraphs(text)
+		
 		text = self.split_text(text)
 		
-		
+		self.sent_count = len(text)
 		
 		keywords = self.grab_keywords(text)
-		
+	
+		#for k in keywords:
+		#	print(k.encode('utf-8'))
+	
 		best_sentences = self.rank_sentences(text, keywords)
 		
 		for index in best_sentences:
-			summary.append(text[index])
+			summary.append(str(text[index]))
 			
-		return summary
+		summary = self.group_summary(summary)
 		
+		return summary
+	
+	def group_summary(self, summary):
+		grouped_summary = []
+		s = ""
+		
+		for sent in summary:
+			if s.find("@") != -1:
+				s = s.replace("@", " ")
+				grouped_summary.append(str(s) + "\n")
+				s = ""
+			s += sent
+			
+		return grouped_summary
+		
+	
 	def split_text(self, text):
 		sentences = []
 		buffer = []
@@ -152,7 +200,7 @@ class Summarizer:
 		sentence_dict = OrderedDict(sorted(sentence_dict.items(), key=lambda t: t[1], reverse=True))
 		
 		for key, value in sentence_dict.items():
-			if count < self.sentence_limit:
+			if count < self.sentence_limit * self.sent_count:
 				best_sentences.append(key)
 			count += 1
 			
@@ -171,14 +219,28 @@ class Summarizer:
 		
 		return rank
 
+	def mark_end_of_paragraphs(self, text):
+		text = text.replace("\n", "@\n")
+		return text
+
+		
 def print_summary(summary):
 	for sent in summary:
-		print(sent)
-		
+		try:
+			print(str(sent))
+		except UnicodeEncodeError:
+			print(str(sent.encode('utf-8')))
+			
 def main():
-	url = "http://www.livescience.com/55973-fda-bans-antibacterial-soap-ingredients.html"
+	#url = "http://www.popsci.com/can-classic-thought-experiment-explain-trumps-win"
 	
-	summarizer = Summarizer(url, 5, 5)
+	url = "http://bleach.wikia.com/wiki/Ichigo_Kurosaki"
+	
+	#url = "http://www.popsci.com/neanderthals-may-have-given-us-genital-warts"
+	
+	#url = "http://bleach.wikia.com/wiki/Aizen"
+	
+	summarizer = Summarizer(url, 25, 0.5)
 	
 	text = summarizer.clean_text()
 	
